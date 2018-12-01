b7cfdb34e0b39c8143cd7fc79622bcb81f5b20a5
E-Liang Tan <eliang@eliangtan.com>
2018-12-01 17:34:13 +0800

    Add custom keymap

---
+++ keyboards/whitefox/keymaps/e-liang/keymap.c
@@ -0,0 +1,54 @@
+/*
+Copyright 2015 Jun Wako <wakojun@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include QMK_KEYBOARD_H
+
+#define _DEFAULT 0
+#define _FN 1
+
+const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* Main layer
+     * ,---------------------------------------------------------------.
+     * |  `|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  [|  ]|  \|   |Fn1|
+     * |---------------------------------------------------------------|
+     * |Tab  |  '|  ,|  .|  P|  Y|  F|  G|  C|  R|  L|  /|  =|Backs|Fn2|
+     * |---------------------------------------------------------------|
+     * |Es/Ctl|  A|  O|  E|  U|  I|  D|  H|  T|  N|  S|  -|Enter   |VoU|
+     * |---------------------------------------------------------------|
+     * |Shif|   |  ;|  Q|  J|  K|  X|  B|  M|  W|  V|  Z|Shift |Up |VoD|
+     * |---------------------------------------------------------------|
+     * |Ctrl|Alt |Gui |         Space         |Gui |Alt |  |Lef|Dow|Rig|
+     * `---------------------------------------------------------------'
+     */
+    [_DEFAULT] = LAYOUT_truefox( \
+      KC_GRV         , KC_1    , KC_2    , KC_3   , KC_4  , KC_5  , KC_6  , KC_7  , KC_8  , KC_9 , KC_0 , KC_LBRC , KC_RBRC , KC_BSLS , KC_GRV  , MO(_FN) , \
+      KC_TAB         , KC_QUOT , KC_COMM , KC_DOT , KC_P  , KC_Y  , KC_F  , KC_G  , KC_C  , KC_R , KC_L , KC_SLSH , KC_EQL  , KC_BSPC           , KC_DEL  , \
+      LCTL_T(KC_ESC) , KC_A    , KC_O    , KC_E   , KC_U  , KC_I  , KC_D  , KC_H  , KC_T  , KC_N , KC_S , KC_MINS , KC_ENT                      , KC_VOLU , \
+      KC_LSFT                  , KC_SCLN , KC_Q   , KC_J  , KC_K  , KC_X  , KC_B  , KC_M  , KC_W , KC_V , KC_Z    , KC_RSFT           , KC_UP   , KC_VOLD , \
+      KC_LCTL        , KC_LALT , KC_LGUI , KC_SPC                                                       , KC_RGUI , KC_RALT , KC_LEFT , KC_DOWN , KC_RGHT \
+    ),
+    [_FN] = LAYOUT_truefox( \
+      _______ , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , KC_F6   , KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_F11  , KC_F12  , _______ , _______ , _______ , \
+      _______ , _______ , KC_BTN1 , KC_BTN2 , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______           , _______ , \
+      _______ , KC_MS_L , KC_MS_D , KC_MS_U , KC_MS_R , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______                     , KC_MPLY , \
+      _______           , KC_WH_L , KC_WH_D , KC_WH_U , KC_WH_R , _______ , _______ , _______ , _______ , _______ , _______ , _______           , KC_PGUP , KC_MUTE , \
+      _______           , _______ , _______ , _______                                                             , _______ , _______ , KC_HOME , KC_PGDN , KC_END  \
+    ),
+};
+
+const uint16_t fn_actions[] = {
+    [0] = ACTION_LAYER_MOMENTARY(1),
+};
