45d064c83dac59474de3009db5ed0be1797baf4f
E-Liang Tan <eliang@eliangtan.com>
2019-04-05 20:16:56 +0800

    Add number row function layer

--- keyboards/whitefox/keymaps/e-liang/keymap.c
+++ keyboards/whitefox/keymaps/e-liang/keymap.c
@@ -19,9 +19,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define _DEFAULT 0
 #define _FN 1
 #define _FN2 2
+#define _FN3 3
 
 enum custom_keycodes {
-  EL_VMBN = SAFE_RANGE,
+  EL_TMWN = SAFE_RANGE,
+  EL_TMWP,
+  EL_VMBN,
   EL_VMBP,
   EL_VMBW,
 };
@@ -29,6 +32,12 @@ enum custom_keycodes {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (record->event.pressed) {
     switch(keycode) {
+      case EL_TMWN:
+        SEND_STRING(SS_LCTRL("b") "n");
+        return false;
+      case EL_TMWP:
+        SEND_STRING(SS_LCTRL("b") "p");
+        return false;
       case EL_VMBN:
         SEND_STRING(":bn\n");
         return false;
@@ -52,17 +61,17 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |---------------------------------------------------------------|
    * |Es/Ctl|  A|  O|  E|  U|  I|  D|  H|  T|  N|  S|  -|Enter   |VoU|
    * |---------------------------------------------------------------|
-   * |Shift   |  ;|  Q|  J|  K|  X|  B|  M|  W|  V|  Z|      |Up |VoD|
+   * |Shift   |  ;|  Q|  J|  K|  X|  B|  M|  W|  V|  Z|Fn2   |Up |VoD|
    * |---------------------------------------------------------------|
-   * |Fn1 |Alt |Gui |         Space         |Gui |Fn2 |  |Lef|Dow|Rig|
+   * |Fn1 |Alt |Gui |         Space         |Gui |Fn3 |  |Lef|Dow|Rig|
    * `---------------------------------------------------------------'
    */
   [_DEFAULT] = LAYOUT_truefox( \
       KC_GRV         , KC_1    , KC_2    , KC_3   , KC_4  , KC_5  , KC_6  , KC_7  , KC_8  , KC_9 , KC_0 , KC_LBRC , KC_RBRC , KC_BSLS , XXXXXXX , KC_PSCR , \
       KC_TAB         , KC_QUOT , KC_COMM , KC_DOT , KC_P  , KC_Y  , KC_F  , KC_G  , KC_C  , KC_R , KC_L , KC_SLSH , KC_EQL  , KC_BSPC           , KC_DEL  , \
       CTL_T(KC_ESC)  , KC_A    , KC_O    , KC_E   , KC_U  , KC_I  , KC_D  , KC_H  , KC_T  , KC_N , KC_S , KC_MINS , KC_ENT                      , KC_VOLU , \
-      KC_LSFT                  , KC_SCLN , KC_Q   , KC_J  , KC_K  , KC_X  , KC_B  , KC_M  , KC_W , KC_V , KC_Z    , XXXXXXXX          , KC_UP   , KC_VOLD , \
-      MO(_FN)        , KC_LALT , KC_LGUI , KC_SPC                                                       , KC_RGUI , MO(_FN2), KC_LEFT , KC_DOWN , KC_RGHT \
+      KC_LSFT                  , KC_SCLN , KC_Q   , KC_J  , KC_K  , KC_X  , KC_B  , KC_M  , KC_W , KC_V , KC_Z    , MO(_FN2)          , KC_UP   , KC_VOLD , \
+      MO(_FN)        , KC_LALT , KC_LGUI , KC_SPC                                                       , KC_RGUI , MO(_FN3), KC_LEFT , KC_DOWN , KC_RGHT \
       ),
 
   /* Function layer 1. The main function layer, with mousekeys, function keys, etc.
@@ -86,24 +95,45 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______           , _______ , _______ , KC_BTN1                                                             , _______ , _______ , KC_HOME , KC_PGDN , KC_END  \
       ),
 
-  /* Function layer 2. Macros.
+  /* Function layer 2. Fn1 modifications.
    * ,---------------------------------------------------------------.
-   * |   |   |   |   |   |   |   |   |   |   |   |   |   |   |Wak|   |
+   * |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
    * |---------------------------------------------------------------|
-   * |     |  `|   |   |   |   |   |   |   |  [|  ]|  \|   |     |   |
+   * |     |   |   |   |   |   |   |   |PgU|   |   |   |   |     |   |
    * |---------------------------------------------------------------|
-   * |Caps  |  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|   |        |Prv|
+   * |      |   |   |   |   |   |   |Hom|PgD|End|   |   |        |   |
    * |---------------------------------------------------------------|
-   * |        |   |   |MVP|MVN|   |   |   |MVW|   |   |      |   |Nxt|
+   * |        |   |   |   |   |   |   |   |   |   |   |      |PgU|   |
+   * |---------------------------------------------------------------|
+   * |    |    |    |                       |    |    |  |Hom|PgD|End|
+   * `---------------------------------------------------------------'
+   */
+  [_FN2] = LAYOUT_truefox( \
+      _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , \
+      _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , KC_PGUP , _______ , _______ , _______ , _______ , _______           , _______ , \
+      _______ , _______ , _______ , _______ , _______ , _______ , _______ , KC_HOME , KC_PGDN , KC_END  , _______ , _______ , _______                     , KC_MPLY , \
+      _______           , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______           , KC_PGUP , KC_MUTE , \
+      _______           , _______ , _______ , _______                                                             , _______ , _______ , KC_HOME , KC_PGDN , KC_END  \
+      ),
+
+  /* Function layer 3. Macros, numbers.
+   * ,---------------------------------------------------------------.
+   * |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
+   * |---------------------------------------------------------------|
+   * |     |  `|   |   |   |   |Del|Bsp|Ent|  [|  ]|  \|   |     |   |
+   * |---------------------------------------------------------------|
+   * |      |  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|   |        |Prv|
+   * |---------------------------------------------------------------|
+   * |        |MTP|MTN|MVP|MVN|   |MVW|   |   |   |   |      |   |Nxt|
    * |---------------------------------------------------------------|
    * |    |    |    |                       |    |    |  |   |   |   |
    * `---------------------------------------------------------------'
    */
-  [_FN2] = LAYOUT_truefox( \
-      _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , KC_WAKE , _______ , \
-      _______ , KC_GRV  , _______ , _______ , _______ , _______ , _______ , _______ , _______ , KC_LBRC , KC_RBRC , KC_BSLS , _______ , _______           , _______ , \
-      KC_CAPS , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    , KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , _______ , _______                     , KC_MPRV , \
-      _______           , _______ , _______ , EL_VMBP , EL_VMBN , _______ , _______ , _______ , EL_VMBW , _______ , _______ , _______           , _______ , KC_MNXT , \
+  [_FN3] = LAYOUT_truefox( \
+      _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , \
+      _______ , KC_GRV  , _______ , _______ , _______ , _______ , KC_DEL  , KC_BSPC , KC_ENT  , KC_LBRC , KC_RBRC , KC_BSLS , _______ , _______           , _______ , \
+      _______ , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    , KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , _______ , _______                     , KC_MPRV , \
+      _______           , EL_TMWP , EL_TMWN , EL_VMBP , EL_VMBN , _______ , EL_VMBW , _______ , _______ , _______ , _______ , _______           , _______ , KC_MNXT , \
       _______           , _______ , _______ , _______                                                             , _______ , _______ , _______ , _______ , _______  \
       ),
 };
